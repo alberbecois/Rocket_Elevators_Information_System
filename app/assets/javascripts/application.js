eb4b5d87aa7459f9be2551033cd1d099b06fb174
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2020-03-04 10:22:50 -0500

    Updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
+//= require bootstrap.min
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require popper
+//= require bootstrap
+//= require jquery_ujs
