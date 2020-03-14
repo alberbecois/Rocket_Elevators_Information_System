0f6d83a66e231a4a3a32e79c2d8f90f6f1627bad
Amuriana <pollicevanessa@gmail.com>
2020-03-13 20:19:47 -0400

    Create Users.rb

---
+++ app/models/Users.rb
@@ -0,0 +1,4 @@
+   Include default devise modules. Others available are:
+   :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
